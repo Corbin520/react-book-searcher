aae493e8e9783269f48844134fbbc18ace0967ec
Corbin Brockbank <corbinbrckbnk76@gmail.com>
2019-06-28 18:07:15 -0600

    deleted card component because im doing it in the search component

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,7 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import Header from "./components/header";
 import Title from "./components/title";
-import Card from "./components/card";
 import Search from "./components/search";
 
 class App extends Component {
